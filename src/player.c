fd48d61a7536e0aaf09f6aaed71137277c25697f
renewablezip <renewablezip01@gmail.com>
2024-04-16 01:30:01 +0200

    Refactor player events and movement

--- src/player.c
+++ src/player.c
@@ -1,57 +1,91 @@
 #include "player.h"
 
-void Player_Events(SDL_Event *event, Player *player) {
-  // switch (event.type) {
-  // case SDL_QUIT:
-  //   closeWindow = true;
-  //   break;
-  // case SDL_KEYDOWN:
-  //   switch (event.key.keysym.scancode) {
-  //   case SDL_SCANCODE_W:
-  //   case SDL_SCANCODE_UP:
-  //     up = true;
-  //     currentRow = 3; // Set to the second row for the left movement
-  //     playerRect.y = currentRow * frameHeight;
-  //     break;
-  //   case SDL_SCANCODE_A:
-  //   case SDL_SCANCODE_LEFT:
-  //     left = true;
-  //     currentRow = 1; // Set to the second row for the left movement
-  //     playerRect.y = currentRow * frameHeight;
-  //     break;
-  //   case SDL_SCANCODE_S:
-  //   case SDL_SCANCODE_DOWN:
-  //     down = true;
-  //     currentRow = 0; // Set to the second row for the left movement
-  //     playerRect.y = currentRow * frameHeight;
-  //     break;
-  //   case SDL_SCANCODE_D:
-  //   case SDL_SCANCODE_RIGHT:
-  //     right = true;
-  //     currentRow = 2; // Set to the second row for the left movement
-  //     playerRect.y = currentRow * frameHeight;
-  //     break;
-  //   }
-  //   break;
-  // case SDL_KEYUP:
-  //   switch (event.key.keysym.scancode) {
-  //   case SDL_SCANCODE_W:
-  //   case SDL_SCANCODE_UP:
-  //     up = false;
-  //     break;
-  //   case SDL_SCANCODE_A:
-  //   case SDL_SCANCODE_LEFT:
-  //     left = false;
-  //     break;
-  //   case SDL_SCANCODE_S:
-  //   case SDL_SCANCODE_DOWN:
-  //     down = false;
-  //     break;
-  //   case SDL_SCANCODE_D:
-  //   case SDL_SCANCODE_RIGHT:
-  //     right = false;
-  //     break;
-  //   }
-  //   break;
-  // }
+#define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 400
+
+SDL_Rect GetPlayerBoundingBox(Player *player) {
+  return (SDL_Rect){
+      player->coordinate.x,
+      player->coordinate.y,
+      player->frame_size.w,
+      player->frame_size.h,
+
+  };
+}
+
+void PlayerEventLoop(Player *player) {
+  if (player->movement_flags.up && !player->movement_flags.down)
+    player->coordinate.y += -player->speed;
+  if (player->movement_flags.down && !player->movement_flags.up)
+    player->coordinate.y += player->speed;
+  if (player->movement_flags.left && !player->movement_flags.right)
+    player->coordinate.x += -player->speed;
+  if (player->movement_flags.right && !player->movement_flags.left)
+    player->coordinate.x += player->speed;
+
+  float newPlayerX = player->coordinate.x;
+  float newPlayerY = player->coordinate.y;
+
+  if (newPlayerX < 0) {
+    newPlayerX = 0;
+  } else if (newPlayerX + player->frame_size.w > WINDOW_WIDTH) {
+    newPlayerX = WINDOW_WIDTH - player->frame_size.w;
+  }
+  if (newPlayerY < 0) {
+    newPlayerY = 0;
+  } else if (newPlayerY + player->frame_size.h > WINDOW_HEIGHT) {
+    newPlayerY = WINDOW_HEIGHT - player->frame_size.h;
+  }
+
+  player->coordinate.x = newPlayerX;
+  player->coordinate.y = newPlayerY;
+}
+
+void PlayerEvents(SDL_Event *event, Player *player, SDL_Rect *nextPlayerFrame) {
+  switch (event->type) {
+  case SDL_KEYDOWN:
+    switch (event->key.keysym.scancode) {
+    case SDL_SCANCODE_W:
+    case SDL_SCANCODE_UP:
+      player->movement_flags.up = true;
+      nextPlayerFrame->y = 3 * player->frame_size.h;
+      break;
+    case SDL_SCANCODE_A:
+    case SDL_SCANCODE_LEFT:
+      player->movement_flags.left = true;
+      nextPlayerFrame->y = 1 * player->frame_size.h;
+      break;
+    case SDL_SCANCODE_S:
+    case SDL_SCANCODE_DOWN:
+      player->movement_flags.down = true;
+      nextPlayerFrame->y = 0 * player->frame_size.h;
+      break;
+    case SDL_SCANCODE_D:
+    case SDL_SCANCODE_RIGHT:
+      player->movement_flags.right = true;
+      nextPlayerFrame->y = 2 * player->frame_size.h;
+      break;
+    }
+    break;
+  case SDL_KEYUP:
+    switch (event->key.keysym.scancode) {
+    case SDL_SCANCODE_W:
+    case SDL_SCANCODE_UP:
+      player->movement_flags.up = false;
+      break;
+    case SDL_SCANCODE_A:
+    case SDL_SCANCODE_LEFT:
+      player->movement_flags.left = false;
+      break;
+    case SDL_SCANCODE_S:
+    case SDL_SCANCODE_DOWN:
+      player->movement_flags.down = false;
+      break;
+    case SDL_SCANCODE_D:
+    case SDL_SCANCODE_RIGHT:
+      player->movement_flags.right = false;
+      break;
+    }
+    break;
+  }
 }
--- src/player.h
+++ src/player.h
@@ -2,6 +2,7 @@
 #define _PLAYER_
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_rect.h>
 #include <stdbool.h>
 
 typedef struct {
@@ -9,9 +10,18 @@ typedef struct {
 } PlayerMovementFlags;
 
 typedef struct {
+  int w, h;
+} Vec2;
+
+typedef struct {
   PlayerMovementFlags movement_flags;
+  SDL_Point coordinate;
+  Vec2 frame_size;
+  int speed;
 } Player;
 
-void Player_Events(SDL_Event *event, Player *player);
+SDL_Rect GetPlayerBoundingBox(Player *player);
+void PlayerEvents(SDL_Event *event, Player *player, SDL_Rect *nextPlayerFrame);
+void PlayerEventLoop(Player *player);
 
 #endif // !_PLAYER_
