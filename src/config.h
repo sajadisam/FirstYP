dcfe8d0a29e3cf74331eddaea79d0378c81bced8
renewablezip <renewablezip01@gmail.com>
2024-04-16 01:33:01 +0200

    Seperate project macro configs

---
+++ src/config.h
@@ -0,0 +1,10 @@
+#ifndef _CONFIG_
+#define _CONFIG_
+
+#define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 400
+#define FRAME_DELAY_MS 100
+#define FPS 60
+#define ARROW_SPEED 200
+
+#endif // !_CONFIG_
