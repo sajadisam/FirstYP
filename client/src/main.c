4b8d098825b2b45c39a52c5cbe311cec9fccded9
renewablezip01 <renewablezip01@gmail.com>
2024-05-02 21:18:23 +0200

    Update main.c

--- client/src/main.c
+++ client/src/main.c
@@ -1,7 +1,34 @@
+#include "../../shared/include/debug.h"
+#include "config.h"
+#include "game.h"
+#include "player/player.h"
+#include "setup.h"
+#include "window/window.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_video.h>
+#include <stdbool.h>
 
-#include <stdio.h>
+/*
+ * A callback that gets triggered based on window event
+ */
+void *window_event_callback(SDL_Event *event, void *arg) {
+  if (event->type == SDL_QUIT) {
+    DEBUG("Closing the window\n");
+    return true;
+  }
+  return false;
+}
 
 int main() {
-  printf("Hello world\n");
-  return 0;
+  initalize_sdl();
+  Window *window = window_create("Zombie hunter", WINDOW_WIDTH, WINDOW_HEIGHT);
+  Player *player = create_player();
+  Game *game = create_game(window, player);
+  while (true) {
+    if (window_event_loop(window, window_event_callback, NULL))
+      break;
+    SDL_RenderClear(get_game_renderer(game));
+    SDL_RenderPresent(get_game_renderer(game));
+  }
 }
