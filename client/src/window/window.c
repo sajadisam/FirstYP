5e713edb0a4688f58da56617df71c2b95899bb80
renewablezip01 <renewablezip01@gmail.com>
2024-05-02 21:17:00 +0200

    Update window wrapper

--- client/src/window/window.c
+++ client/src/window/window.c
@@ -1,5 +1,6 @@
 #include "../../../shared/include/debug.h"
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_video.h>
@@ -9,6 +10,12 @@ typedef struct {
   SDL_Window *m_Window;
   SDL_Renderer *m_Renderer;
 } Window;
+
+typedef void *(*EventLoopCallback)(SDL_Event *event, void *);
+
+/* GETTERS & SETTERS */
+SDL_Renderer *get_window_renderer(Window *window) { return window->m_Renderer; }
+SDL_Window *get_window_sdlwindow(Window *window) { return window->m_Window; }
 
 // TODO(Xelian): move to shared
 void on_sdl_error() {
@@ -20,8 +27,9 @@ void on_sdl_error() {
 Window *window_create(const char *name, int width, int height) {
   INFO("Creating a window\n");
 
-  SDL_Window *pWindow = SDL_CreateWindow(
-      name, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0);
+  SDL_Window *pWindow =
+      SDL_CreateWindow(name, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                       width, height, SDL_WINDOW_RESIZABLE);
 
   if (!pWindow)
     on_sdl_error();
@@ -59,4 +67,15 @@ SDL_Texture *create_texture(Window *wnd, const char *path) {
     on_sdl_error();
 
   return pTexture;
+}
+
+void *window_event_loop(Window *window, EventLoopCallback callback, void *arg) {
+  SDL_Event event;
+  while (SDL_PollEvent(&event) > 0) {
+    void *callbackResult = callback(&event, arg);
+    if (callbackResult) {
+      return callbackResult;
+    }
+  }
+  return NULL;
 }
--- client/src/window/window.h
+++ client/src/window/window.h
@@ -1,12 +1,30 @@
 #ifndef _WINDOW_
 #define _WINDOW_
 
+#include <SDL2/SDL_events.h>
 #include <SDL2/SDL_render.h>
 
 typedef struct window Window;
 
+SDL_Renderer *get_window_renderer(Window *window);
+SDL_Window *get_window_sdlwindow(Window *window);
+
+typedef void *(*EventLoopCallback)(SDL_Event *event, void *);
+
 Window *window_create(const char *name, int width, int height);
+
 void window_destroy(Window *window);
+
 SDL_Texture *create_texture(Window *wnd, const char *path);
+
+/*
+ *  Handles a single window event by passing it to the callback
+ *
+ *  @arg window: Window to listen on the events
+ *  @arg callback: callback to send the events to
+ *  @arg arg: additional argument to be passed to the callback
+ *  @return the value
+ */
+void *window_event_loop(Window *window, EventLoopCallback callback, void *arg);
 
 #endif // !_WINDOW_
