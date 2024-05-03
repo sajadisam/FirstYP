c93c612ea57b9f5b6cf2483daa4f5787435bfafe
renewablezip01 <renewablezip01@gmail.com>
2024-05-03 19:42:44 +0200

    Update main.c

--- client/src/main.c
+++ client/src/main.c
@@ -1,26 +1,24 @@
 #include "game.h"
 #include "player/player.h"
-#include "setup.h"
 #include "shared/config.h"
 #include "ui/sprite.h"
+#include "ui/spritesheet.h"
 #include "window/window.h"
 #include "window/window_event_callback.h"
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_rect.h>
 #include <SDL2/SDL_render.h>
-#include <SDL2/SDL_video.h>
-#include <stdbool.h>
 
 int main() {
-  initalize_sdl();
   Window *window = window_create("Zombie hunter", WINDOW_WIDTH, WINDOW_HEIGHT);
   Player *player = create_player();
-  Sprite *sprite =
-      create_sprite(window, "./resources/player.png", (SDL_Point){100, 100});
+  SpriteSheet *playerSprite =
+      create_spritesheet(window, "./resources/old_player.png", 3, 3);
+  Sprite *mapSprite = create_sprite(window, "./resources/map.png");
   Game *game = create_game(window, player);
-  while (true) {
-    if (window_event_loop(window, window_event_callback, NULL))
-      break;
-    render_sprite_loop(game, sprite);
+  while (!window_event_loop(window, window_event_callback, game)) {
+    SDL_RenderClear(get_game_renderer(game));
+    render_sprite(game, mapSprite, (Size){0, 0}, (SDL_Point){0, 0});
+    render_spritesheet(game, playerSprite, (Size){10, 10}, (SDL_Point){20, 20});
+    SDL_RenderPresent(get_game_renderer(game));
   }
 }
