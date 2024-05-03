f7f1bd7b6484a2645c649b7b5cadc47c77466d35
renewablezip01 <renewablezip01@gmail.com>
2024-05-03 21:58:22 +0200

    Update game

--- client/src/game.c
+++ client/src/game.c
@@ -1,25 +1,28 @@
+#include "clock.h"
 #include "player/player.h"
+#include "ui/sprite.h"
 #include "window/window.h"
-#include <stdlib.h>
 
 typedef struct {
   Window *window;
   Player *player;
+  Sprite *map;
+  Clock *clock;
 } Game;
-
-/* GETTERS & SETTERS */
 
 SDL_Renderer *get_game_renderer(Game *game) {
   return get_window_renderer(game->window);
 }
+Window *get_game_window(Game *game) { return game->window; }
+Player *get_game_player(Game *game) { return game->player; }
+Sprite *get_game_map(Game *game) { return game->map; }
+Clock *get_game_clock(Game *game) { return game->clock; }
 
-SDL_Window *get_game_sdlwindow(Game *game) {
-  return get_window_sdlwindow(game->window);
-}
-
-Game *create_game(Window *window, Player *player) {
+Game *create_game(Window *window, Player *player, Sprite *map, Clock *clock) {
   Game *game = malloc(sizeof(Game));
   game->window = window;
   game->player = player;
+  game->map = map;
+  game->clock = clock;
   return game;
 }
--- client/src/game.h
+++ client/src/game.h
@@ -1,6 +1,7 @@
 #ifndef _GAME_
 #define _GAME_
 
+#include "clock.h"
 #include "player/player.h"
 #include "window/window.h"
 #include <SDL2/SDL_render.h>
@@ -14,12 +15,16 @@ enum GameState {
 // Struct that defines our overall game data
 typedef struct game Game;
 
-SDL_Renderer *get_game_renderer(Game *game);
-SDL_Window *get_game_sdlwindow(Game *game);
-
 /*
  * Create an instance of our game
  */
-Game *create_game(Window *window, Player *player);
+Game *create_game(Window *window, Player *player, struct Sprite *map,
+                  Clock *clock);
+
+SDL_Renderer *get_game_renderer(Game *game);
+Window *get_game_window(Game *game);
+Player *get_game_player(Game *game);
+Clock *get_game_clock(Game *game);
+struct Sprite *get_game_map(Game *game);
 
 #endif // !_GAME_
