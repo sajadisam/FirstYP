d52f55d5223e5d781b4b58e81756f9c2d3f9b5ae
renewablezip01 <renewablezip01@gmail.com>
2024-05-07 00:33:52 +0200

    Initialize SDL TTF

--- client/src/main.c
+++ client/src/main.c
@@ -2,11 +2,13 @@
 #include "SDL2/SDL_net.h"
 #include "debug.h"
 #include "game.h"
+#include <SDL2/SDL_ttf.h>
 
 #undef main
 
 int main() {
-  if (SDL_Init(SDL_INIT_EVERYTHING) == -1 || (SDLNet_Init() == -1)) {
+  if (SDL_Init(SDL_INIT_EVERYTHING) == -1 || (SDLNet_Init() == -1) ||
+      TTF_Init() == -1) {
     ERR("Failed to initialize SDL\n");
     exit(1);
   }
