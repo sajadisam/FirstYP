c9a479bfc5a1189b7d13c90cc5e6607c57c95c34
renewablezip01 <renewablezip01@gmail.com>
2024-05-07 00:32:51 +0200

    Create generic UI component

--- client/src/ui/element.h
+++ client/src/ui/element.h
@@ -5,7 +5,9 @@
 
 typedef struct Element Element;
 
-Element *element_create(SDL_Rect rect);
+Element *element_create(SDL_Rect rect, void (*draw)(void *component),
+                        void *component);
+
 void element_destroy(Element *element);
 
 void element_draw(Element *element);
@@ -13,5 +15,6 @@ void element_draw(Element *element);
 SDL_Rect element_get_rect(Element *element);
 void element_set_rect(Element *element, SDL_Rect rect);
 void element_set_coordinate(Element *element, SDL_Point rect);
+void *element_get_coomponent(Element *element);
 
 #endif // !_Element_
