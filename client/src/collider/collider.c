20aa7c97702aaaad5e3a1ce841f66d1136a32a7c
renewablezip01 <renewablezip01@gmail.com>
2024-05-17 00:16:41 +0200

    Turn collider into an entity

--- client/src/collider/collider.c
+++ client/src/collider/collider.c
@@ -1,51 +1,61 @@
+#include "../entity/entity.h"
 #include "collider_common.h"
 #include <SDL2/SDL_rect.h>
 #include <stdbool.h>
 
 typedef struct {
-  SDL_Rect rect;
+  Entity *entity;
+  SDL_Point size;
   ColliderType type;
-  void *entity;
+  void *target;
   OnCollisionFunc on_collision;
 } Collider;
 
 Collider *collider_create(SDL_Rect rect, ColliderType type, void *entity,
                           OnCollisionFunc on_collision) {
   Collider *collider = malloc(sizeof(Collider));
-  collider->rect = rect;
+  collider->entity = entity_create();
+  entity_set_coord(collider->entity, (SDL_Point){rect.x, rect.x});
+  collider->size.x = rect.w;
+  collider->size.y = rect.h;
   collider->type = type;
-  collider->entity = entity;
+  collider->target = entity;
   collider->on_collision = on_collision;
   return collider;
 }
 
-void collider_destroy(Collider *collider) { free(collider); }
+void collider_destroy(Collider *collider) {
+  entity_destroy(collider->entity);
+  free(collider);
+}
+
+SDL_Point collider_get_size(Collider *collider) { return collider->size; }
 
 void collider_set_size(Collider *collider, int w, int h) {
-  collider->rect = (SDL_Rect){collider->rect.x, collider->rect.y, w, h};
+  collider->size.x = w;
+  collider->size.y = h;
 }
 
 void collider_set_coordinate(Collider *collider, int x, int y) {
-  collider->rect.x = x;
-  collider->rect.y = y;
+  entity_set_coord(collider->entity, (SDL_Point){x, y});
 }
 
-void collider_set_rect(Collider *collider, SDL_Rect rect) {
-  collider->rect = rect;
-}
-
-void *collider_get_entity(Collider *collider) { return collider->entity; }
+void *collider_get_target(Collider *collider) { return collider->target; }
+Entity *collider_get_entity(Collider *collider) { return collider->entity; }
 ColliderType collider_get_type(Collider *collider) { return collider->type; }
-SDL_Rect collider_get_rect(Collider *collider) { return collider->rect; }
 
 bool collider_check(Collider *a, Collider *b) {
-  return SDL_HasIntersection(&a->rect, &b->rect);
+  SDL_Point coordA = entity_get_coord(a->entity);
+  SDL_Rect rectA = (SDL_Rect){coordA.x, coordA.y, a->size.x, a->size.y};
+  SDL_Point coordB = entity_get_coord(b->entity);
+  SDL_Rect rectB = (SDL_Rect){coordB.x, coordB.y, b->size.x, b->size.y};
+  return SDL_HasIntersection(&rectA, &rectB);
 }
 
 bool collider_execute(Collider *a, Collider *b) {
   if (collider_check(a, b)) {
-    a->on_collision(a->entity, b);
-    b->on_collision(b->entity, a);
+    a->on_collision(a->target, b);
+    b->on_collision(b->target, a);
     return true;
   }
   return false;
--- client/src/collider/collider.h
+++ client/src/collider/collider.h
@@ -1,6 +1,7 @@
 #ifndef _COLLIDER_
 #define _COLLIDER_
 
+#include "../entity/entity.h"
 #include "collider_common.h"
 #include <SDL2/SDL_rect.h>
 #include <stdbool.h>
@@ -13,9 +14,10 @@ int collider_check(Collider *a, Collider *b);
 void collider_set_size(Collider *collider, int w, int h);
 void collider_set_coordinate(Collider *collider, int x, int y);
 void collider_set_rect(Collider *collider, SDL_Rect rect);
-void *collider_get_entity(Collider *collider);
+void *collider_get_target(Collider *collider);
+Entity *collider_get_entity(Collider *collider);
 ColliderType collider_get_type(Collider *collider);
-SDL_Rect collider_get_rect(Collider *collider);
 bool collider_execute(Collider *a, Collider *b);
+SDL_Point collider_get_size(Collider *collider);
 
 #endif // !_COLLIDER_
