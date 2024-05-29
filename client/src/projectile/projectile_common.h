f744010fbc9627a444a8304afe48620d7834426a
adamscoding123 <alashiriadam@gmail.com>
2024-05-29 21:18:27 +0200

    Add projectile types

---
+++ client/src/projectile/projectile_common.h
@@ -0,0 +1,10 @@
+#ifndef _PROJECTILE_COMMON_
+#define _PROJECTILE_COMMON_
+
+typedef void (*ProjectileDestroyFunc)(void *);
+
+typedef enum {
+  PROJECTILE_TOMATO,
+} ProjectileType;
+
+#endif // !_PROJECTILE_COMMON_
