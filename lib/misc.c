6a3f01e991465adf4e7ebdd4dd90cf2ee3930836
renewablezip01 <renewablezip01@gmail.com>
2024-06-13 22:17:26 +0200

    Add math functions to shared library

--- lib/misc.c
+++ lib/misc.c
@@ -8,3 +8,10 @@ void on_sdl_error() {
 }
 
 int randomi32(int min, int max) { return rand() % (max + 1 - min) + min; }
+
+float clamp(float value, float min, float max) {
+  const double t = value < min ? min : value;
+  return t > max ? max : t;
+}
+
+float vector_length(int x, int y) { return sqrt(x * x + y * y); }
--- lib/misc.h
+++ lib/misc.h
@@ -4,4 +4,7 @@
 void on_sdl_error();
 int randomi32(int min, int max);
 
+float clamp(float value, float min, float max);
+float vector_length(int x, int y);
+
 #endif // !_MISC_
